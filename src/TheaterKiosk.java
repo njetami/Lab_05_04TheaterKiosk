f3a506b624deef4e8f1dcfef91eaff58abe47495
Njeta Melvin Ndi <njetami@mail.uc.edu>
2022-06-20 08:07:47 -0400

    Initial commit

---
+++ src/TheaterKiosk.java
@@ -0,0 +1,16 @@
+public class TheaterKiosk {
+    public static void main(String[] args) {
+   int WRISTBAND_AGE= 21;
+   int userAge = 5;
+
+   if(userAge>=WRISTBAND_AGE)
+   {
+       System.out.println("you get a Paper wristband");
+   }
+    else
+    {
+        System.out.println("No Wristband");
+    }
+
+    }
+}
